60bd80946debe19e52c4d66d811cbbd07658e87e
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2018-12-06 09:56:56 +0100

    Initial commit

---
+++ PP04_pseudo-periode.py
@@ -0,0 +1,15 @@
+# Pour la lecture
+import numpy as np
+# Lecture des donnees
+donnees_t,donnees_u = np.loadtxt('Uc_11.txt',unpack=True)
+
+# Trouvez le moyen de determiner la periode des passages par 0 (dans le meme 
+# sens) en vous appuyant sur les nb premiers passages par 0.
+def periode(t,u,nb=10):
+    return 0
+    
+    
+# Reste a afficher le resultat apres application de la fonction sur les donnees
+pseudo_periode = 'Pensez a appliquer votre fonction'
+
+print(pseudo_periode)
